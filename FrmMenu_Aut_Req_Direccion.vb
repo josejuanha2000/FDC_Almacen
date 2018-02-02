b413ecd8f1917649ac46e4271815b4d86656851f
juan.hernandez <josejuanha@hotmail.com>
2018-02-01 19:29:38 -0600

    Sistema de Almacen

---
+++ FrmMenu_Aut_Req_Direccion.vb
@@ -0,0 +1,14 @@
+﻿Public Class FrmMenu_Aut_Req_Direccion
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FRMREQMAT.Show()
+        'Hide()
+    End Sub
+    Private Sub BtnAutoriza_Click(sender As Object, e As EventArgs) Handles BtnAutoriza.Click
+        FrmJefes.Show()
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        End
+    End Sub
+End Class
